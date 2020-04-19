e1b5a310a0af5d3c870a53dab72229f68fed352b
Exo Sol <1.petarzlatar@gmail.com>
2020-04-19 17:00:20 +0200

    done chart globaly now how to cut for every Pon to Ned

--- js/global.js
+++ js/global.js
@@ -32,8 +32,89 @@ byDate.sort(function (a, b) {
 });
 console.log("by date:");
 console.log(byDate);
+/////////////////////////////////Make chart by days of month
+getMakeChart(byDate);
 ////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////CHART BY MONTH ///////////////////////////////////////////
+function getMakeChart(item) {
+  console.log(item);
+  // array of tot mancha, time pairs
+  var times = [],
+    mTotcur = [],
+    mTot = [],
+    reducer,
+    x,
+    y;
+  item.forEach((ele, ind1) => {
+    ele.mancha.forEach((ele) => {
+      console.log(ele);
+      reducer = (acc, cur) => acc + cur;
+      x = ele.reduce(reducer);
+      console.log(ind1);
+      mTotcur.push(x);
+
+      console.log(mTot);
+      console.log("comma");
+    });
+    y = mTotcur.reduce(reducer);
+    mTot.push(y);
+    mTotcur = [];
+  });
+
+  item.forEach((ele) => {
+    times.push([ele.diW, ele.date]);
+  });
+
+  console.log(mTot);
+  console.log(times);
+  document.querySelector(".containerCart").style.display = "block";
+  document.getElementById("myChart").style.display = "block";
+
+  let myChart = document.getElementById("myChart").getContext("2d");
+  console.log(myChart);
+
+  var infoChart = new Chart(myChart, {
+    type: "bar",
+    data: {
+      labels: times,
+      datasets: [
+        {
+          label: "Mancha day of month",
+
+          data: mTot,
+          backgroundColor: "#007aa5",
+          borderWidth: 2,
+          borderColor: "#007aa5",
+        },
+      ],
+    },
+    options: {
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: {
+              display: false,
+            },
+            ticks: {
+              fontColor: "#687d8b", // this here
+            },
+          },
+        ],
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+              fontColor: "#21abcd",
+            },
+          },
+        ],
+      },
+    },
+  });
+}
 
 //////////////////////////////////////////// function for day of week
 
