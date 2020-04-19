ef6356d83ca06afa32e4dda189130e99189d0093
Exo Sol <1.petarzlatar@gmail.com>
2020-04-19 15:02:57 +0200

    sorted data for month chart, corrected getDate

--- js/global.js
+++ js/global.js
@@ -1,20 +1,53 @@
 const secondPage = 3;
+///////////////DATA///////////////////////////////////////// DATA
+let completeData = [],
+  data,
+  dateKey,
+  weekDay;
 
-let dates = [], manchas = [], tempMancha = [];
+let storage = window.localStorage;
 
-
- let storage = window.localStorage;
-
-
- 
 for (var key in storage) {
-    if (storage.hasOwnProperty(key)) {
-        dates.push(key);
-        console.log(key);
-        
-        let data = JSON.parse(storage[key]);
-        console.log(data);
-       
-        //console.log(key + " -> " + storage[key]);
-    }
+  if (storage.hasOwnProperty(key)) {
+    dateKey = parseFloat(key);
+    weekDay = dayofWeek(key);
+
+    data = JSON.parse(storage[key]);
+
+    data.date = dateKey;
+    // day in week
+    data.diW = weekDay;
+    console.log(data);
+    completeData.push(data);
+
+    //console.log(key + " -> " + storage[key]);
+  }
 }
+
+console.log(completeData);
+
+var byDate = completeData.slice(0);
+byDate.sort(function (a, b) {
+  return a.date - b.date;
+});
+console.log("by date:");
+console.log(byDate);
+////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////CHART BY MONTH ///////////////////////////////////////////
+
+//////////////////////////////////////////// function for day of week
+
+function dayofWeek(date) {
+  let dateSplitter = date.split(".");
+
+  let dayTarget = dateSplitter[0];
+  let monthTarget = dateSplitter[1];
+
+  //hardcode for 2019
+  var d = new Date(2019, monthTarget, dayTarget);
+  var n = d.getDay();
+  const weekDays = ["Ned", "Pon", "Uto", "Sri", "Cet", "Pet", "Sub"];
+
+  return weekDays[n];
+}
+///////////////////////////////////////////////////////////////
